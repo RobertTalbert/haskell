7f596c9fffb51ae7c11cd87b27ebfc57c43716c8
Robert Talbert <talbertr@gvsu.edu>
2017-07-06 12:05:00 -0400

    Add Chapter 8 exercises

---
+++ recSum.hs
@@ -0,0 +1,23 @@
+recSum :: (Eq a, Num a) => a -> a 
+recSum n
+   | n == 0    = 0 
+   | otherwise = n + recSum(n-1)
+
+recProd :: (Integral a) => a -> a -> a 
+recProd a 0 = 0 
+recProd a b = a + recProd a (b-1) 
+-- I messed up on this by not parenthesizing the b-1. 
+
+recProd' :: (Integral a) => a -> a -> a 
+recProd' a b 
+   | b == 0    = 0 
+   | otherwise = a + recProd a (b-1)
+
+
+
+
+-- recProd a b 
+--    | b == 0    = 0
+--    | otherwise = a + recProd a b-1
+
+
